e89837603111ace585ec80099c65f8baa0651677
aliosman21 <amrali975@gmail.com>
2021-04-11 04:09:58 +0200

    added svg and typist to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,6 +14,7 @@ import {
    MDBDropdownItem,
 } from "mdbreact";
 import Sound from "../assets/yo.mp3";
+import Typist from "react-text-typist";
 import "../styles/navbar.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -42,29 +43,59 @@ class NavbarPage extends Component {
             className="navbarTransparent"
             sticky="top">
             <MDBNavbarBrand>
-               <strong className="white-text">Ali Osman</strong>
+               <Typist
+                  showCursor={false}
+                  typingSpeed="100"
+                  style={{ fontWeight: "bold" }}
+                  sentences={["Ali Osman"]}
+                  loop={false}
+               />
             </MDBNavbarBrand>
             <MDBNavbarToggler onClick={this.toggleCollapse} />
             <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
                <MDBNavbarNav left>
                   <MDBNavItem>
                      <MDBNavLink className="fancy-link pb-0 mb-2" to="/">
-                        Index
+                        <Typist
+                           showCursor={false}
+                           startDelay="900"
+                           typingSpeed="100"
+                           sentences={["Index"]}
+                           loop={false}
+                        />
                      </MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
                      <MDBNavLink to="/about" className="fancy-link pb-0 mb-2">
-                        About Me
+                        <Typist
+                           showCursor={false}
+                           startDelay="1400"
+                           typingSpeed="100"
+                           sentences={["About"]}
+                           loop={false}
+                        />
                      </MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
                      <MDBNavLink to="/home" className="fancy-link pb-0 mb-2">
-                        Works
+                        <Typist
+                           showCursor={false}
+                           startDelay="1900"
+                           typingSpeed="100"
+                           sentences={["Works"]}
+                           loop={false}
+                        />
                      </MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
                      <MDBNavLink to="/login" className="fancy-link pb-0 mb-2">
-                        Login Form
+                        <Typist
+                           showCursor={false}
+                           startDelay="2400"
+                           typingSpeed="100"
+                           sentences={["Login Form"]}
+                           loop={false}
+                        />
                      </MDBNavLink>
                   </MDBNavItem>
                </MDBNavbarNav>
